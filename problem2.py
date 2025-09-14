9a90a715e81ff52b008e37d474c17efcbc6b5eaa
Jaisuryan_07 <jaisuryan0708@gmail.com>
2025-09-14 21:17:08 +0530

    Update problem2.py
    added changes

--- problem2.py
+++ problem2.py
@@ -1,8 +1,17 @@
 # Problem2: Return the second largest unique number in the list.
 
+#def second_largest(nums):
+    #nums = list(set(nums))
+    #nums.sort()
+    #return nums[-2]
+
+#print(second_largest([4, 1, 7, 7, 3]))
+
 def second_largest(nums):
     nums = list(set(nums))
+    if len(nums) < 2:
+        return None  # or raise ValueError("Not enough unique elements")
     nums.sort()
     return nums[-2]
 
-print(second_largest([4, 1, 7, 7, 3]))
+print(second_largest([4, 1, 7, 7, 3]))  # Output: 4
